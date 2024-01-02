22104ddb9b3b05333c5fb45b8b23dca6372dce43
Lucas Alves <lucasalves.12@live.com>
2024-01-02 17:18:37 -0300

    first commit

---
+++ src/test/java/br/com/vogelsapproximationmethod/VogelsApproximationMethodApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.vogelsapproximationmethod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VogelsApproximationMethodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
